75986961d10e7ccc5ba29f45deb9a51bac21c80b
EdouardY <yuedouard@gmail.com>
2024-05-28 11:43:50 +0200

    fix home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,10 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent } from "react";
 import "@/pages/home/Home.css";
 
 const Home: FunctionComponent = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Home Page</h1>
     </>
   );
 };
